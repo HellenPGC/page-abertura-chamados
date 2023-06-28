9acb1a057111b4812bbffeacef11f0b5e16df40c
Hellen <hp_garcia@hotmail.com>
2023-06-28 16:36:48 -0300

    meu projeto SASS

---
+++ src/componentes/Menu.jsx
@@ -0,0 +1,36 @@
+//cards do boostrap:
+
+function Menu() {
+    return (
+
+        <div className='containerNav'>
+            <div className="row">
+                <div className="col-12">
+
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+</div>
+</div>
+</div>
+          
+
+    );
+};
+
+
+export default Menu
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Solicitacao from './ui/Solicitacao.jsx'
-import HomeCard from './ui/HomeCard.jsx'
 import App from './App.jsx'
+import "./assets/bootstrap-personalizado.scss"
 
-const rotas = createBrowserRouter([
-{
-  path: "/",
-  element: <App />,
-  children: [
-
-{
-  path: "/solicitacao",
-  element: <Solicitacao/>
-},
-
-{
-path: "/",
-element: <App/>
-},
-
-  ],
-},
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render( 
   
   <React.StrictMode>
-    <RouterProvider router={rotas} />
-</React.StrictMode>
-);
+<App />
+
+</React.StrictMode>,
+
+)
