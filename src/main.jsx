e27e08d3821ee32f88db6004b903e6ced909b033
Hellen <hp_garcia@hotmail.com>
2023-06-30 09:38:32 -0300

    rotas incluidas

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/bootstrap-personalizado.scss"
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Sobre from "./pages/Sobre.jsx"
+import Clientes from './pages/Clientes.jsx'
+
+
+const rotas = createBrowserRouter([   
+
+  {
+    path: '/sobre-nos',
+    element: <Sobre/>
+  },
+  
+  {
+    path: '/nossos-clientes',
+    element: <Clientes/>
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
   
   <React.StrictMode>
-<App />
 
-</React.StrictMode>,
+  <RouterProvider router={rotas}>
+  </RouterProvider>
+
+</React.StrictMode>
 
 )
